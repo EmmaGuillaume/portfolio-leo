ae2b0b23aada8677080ec0377c93d15d85d49d83
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2025-03-04 10:01:25 +0100

    ✨ Add competence pages and footer component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="w-full px-16 py-2 flex justify-between sticky border-b border-gray-100">
+    <div className="w-full px-16 py-2 flex justify-between fixed top-0 bg-black border-b border-gray-100 z-50">
       <Link href="/">Léo Bournizien</Link>
-      <div className="flex space-x-4">
-        <Link href="/presentation">Présentation</Link>
-        <Link href="">2</Link>
-        <Link href="">3</Link>
+      <div className="flex space-x-4 ">
+        <Link href="/presentation" className="hover:underline">
+          Présentation
+        </Link>
+        <Link href="" className="hover:underline">
+          Réalisations
+        </Link>
+        <Link href="" className="hover:underline">
+          Compétences
+        </Link>
       </div>
     </div>
   );
